a5501fd85b4da082c674b7ae1ccf6a7c5e79f1a6
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-02-08 18:06:59 +0300

    adds JWTAuthorization middleware

--- src/api/lib/jwt-tools.js
+++ src/api/lib/jwt-tools.js
@@ -12,3 +12,16 @@ export const createAccessToken = (payload) =>
       }
     );
   });
+
+export const verifyAccessToken = (token) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { expiresIn: "1 week" },
+      (err, originalPayload) => {
+        if (err) reject(err);
+        else resolve(originalPayload);
+      }
+    );
+  });
--- src/api/me/index.js
+++ src/api/me/index.js
@@ -2,12 +2,28 @@ import express from "express";
 import basicAuthentication from "../lib/auth/basicAuth.js";
 import BlogPostModel from "../blogPosts/model.js";
 import meOnlyMiddleware from "../lib/auth/meOnly.js";
+import { JWTAuthorization } from "../lib/auth/jwtAuth.js";
 
 const meRouter = express.Router();
 
+// meRouter.get(
+//   "/stories",
+//   basicAuthentication,
+//   meOnlyMiddleware,
+//   async (req, res, next) => {
+//     try {
+//       const authorId = req.author._id.toString();
+//       const blogPosts = await BlogPostModel.find({ authors: authorId });
+//       res.send(blogPosts);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
+
 meRouter.get(
   "/stories",
-  basicAuthentication,
+  JWTAuthorization,
   meOnlyMiddleware,
   async (req, res, next) => {
     try {
@@ -19,5 +35,4 @@ meRouter.get(
     }
   }
 );
-
 export default meRouter;
